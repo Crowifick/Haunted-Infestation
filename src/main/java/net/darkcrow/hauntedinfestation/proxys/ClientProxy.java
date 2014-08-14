cf97061c2e241e428f65553200c9373c4a97b643
Darkhax <darklime@live.ca>
2014-08-13 22:45:50 -0600

    Added renders.

--- src/main/java/net/darkcrow/hauntedinfestation/proxys/ClientProxy.java
+++ src/main/java/net/darkcrow/hauntedinfestation/proxys/ClientProxy.java
@@ -1,5 +1,15 @@
 package net.darkcrow.hauntedinfestation.proxys;
 
+import net.darkcrow.hauntedinfestation.entitys.EntitySoul;
+import net.darkcrow.hauntedinfestation.renderer.entity.RenderSoul;
+import net.darkcrow.hauntedinfestation.renderer.models.ModelSoul;
+import cpw.mods.fml.client.registry.RenderingRegistry;
+
 public class ClientProxy extends CommonProxy {
     
+    @Override
+    public void registerRenders () {
+    
+        RenderingRegistry.registerEntityRenderingHandler(EntitySoul.class, new RenderSoul(new ModelSoul(), 1.0F));
+    }
 }
--- src/main/java/net/darkcrow/hauntedinfestation/proxys/CommonProxy.java
+++ src/main/java/net/darkcrow/hauntedinfestation/proxys/CommonProxy.java
@@ -2,4 +2,7 @@ package net.darkcrow.hauntedinfestation.proxys;
 
 public class CommonProxy {
     
+    public void registerRenders () {
+    
+    }
 }
