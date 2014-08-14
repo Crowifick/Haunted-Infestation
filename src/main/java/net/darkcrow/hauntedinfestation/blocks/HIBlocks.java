ba39533b8c92df493b6f29dc66d15b9490c6d79b
Darkhax <darklime@live.ca>
2014-08-13 21:59:58 -0600

    Fixed object handler

--- src/main/java/net/darkcrow/hauntedinfestation/blocks/HIBlocks.java
+++ src/main/java/net/darkcrow/hauntedinfestation/blocks/HIBlocks.java
@@ -3,7 +3,6 @@ package net.darkcrow.hauntedinfestation.blocks;
 import net.darkcrow.hauntedinfestation.util.Reference;
 import cpw.mods.fml.common.registry.GameRegistry;
 
-@GameRegistry.ObjectHolder(Reference.MODID)
 public class HIBlocks {
     
     public static void init () {
